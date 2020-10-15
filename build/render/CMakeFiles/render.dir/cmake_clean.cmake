5ba144ac8575cd638542d8c0da965177de91873e
liangjun.cao <yq_caoforever@163.com>
2020-10-15 16:08:10 +0800

    change the file tree logic

---
+++ build/render/CMakeFiles/render.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/render.dir/src/Ball.cpp.obj"
+  "CMakeFiles/render.dir/src/Camera.cpp.obj"
+  "CMakeFiles/render.dir/src/Quad.cpp.obj"
+  "CMakeFiles/render.dir/src/SceneObj.cpp.obj"
+  "CMakeFiles/render.dir/src/SkyBox.cpp.obj"
+  "CMakeFiles/render.dir/src/TextRender.cpp.obj"
+  "CMakeFiles/render.dir/src/element.cpp.obj"
+  "CMakeFiles/render.dir/src/entrance.cpp.obj"
+  "CMakeFiles/render.dir/src/shader.cpp.obj"
+  "CMakeFiles/render.dir/src/stb_image.cpp.obj"
+  "CMakeFiles/render.dir/src/terrain.cpp.obj"
+  "librender.dll.a"
+  "render.exe"
+  "render.exe.manifest"
+  "render.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/render.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
